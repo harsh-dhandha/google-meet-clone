cc795ca081370a064cc68f6e90b3a87ad1159c55
12erp0 <138237251+12erp0@users.noreply.github.com>
2024-02-15 16:47:28 +0530

    Commit 1 and 2 stashed

---
+++ hooks/usePeer.js
@@ -0,0 +1,11 @@
+const { useState } = required("react")
+
+const usePeer = () => {
+    const [peer, setPeer] = useState(null)
+    cost [myId, setMyId] = useState('')
+
+    useEffect(() => {
+
+
+    },[])
+}
