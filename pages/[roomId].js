1b0bbd1255f773447178226913a2c26e79e8c9b9
12erp0 <138237251+12erp0@users.noreply.github.com>
2024-03-02 22:01:10 +0530

    Made the peer id for join and create room, made its Ui

---
+++ pages/[roomId].js
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+import { useSocket } from "@/context/socket"
+import usePeer from "@/hooks/usePeer"
+
+const Room = () => {
+    const socket =  useSocket()
+   const { peer,myId }=  usePeer();
+};
+
+export default Room;
